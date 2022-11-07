c9ac127a2da49cce4325f35328b761853d60a0c3
inmni <1342885270@qq.com>
2022-11-07 20:41:55 +0800

    修复long->int数据溢出问题

--- src/main/java/io/taraxacum/finaltech/setup/SetupUtil.java
+++ src/main/java/io/taraxacum/finaltech/setup/SetupUtil.java
@@ -475,7 +475,7 @@ public final class SetupUtil {
                 FinalTechItems.ENTROPY,
                 FinalTechItems.ENTROPY_CONSTRUCTOR,
                 FinalTechItems.ENTROPY_SEED);
-        ResearchUtil.setResearches(FinalTech.getLanguageManager(), "BOX", ((int)FinalTech.getSeed()) % 20, false,
+        ResearchUtil.setResearches(FinalTech.getLanguageManager(), "BOX", (((int)FinalTech.getSeed()) % 20) &0x7fffffff, false,
                 FinalTechItems.BOX,
                 FinalTechItems.SHINE);
         ResearchUtil.setResearches(FinalTech.getLanguageManager(), "ANNULAR", (int)Math.pow(ConstantTableUtil.ITEM_COPY_CARD_AMOUNT, 0.25), true,
