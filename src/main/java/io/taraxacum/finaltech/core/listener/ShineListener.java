3c0e5839f50d39554d45f83b0247c9609e2a9e1a
ljc0424 <1979328077@qq.com>
2022-11-11 22:03:27 +0800

    balance tweak for shine

--- src/main/java/io/taraxacum/finaltech/core/listener/ShineListener.java
+++ src/main/java/io/taraxacum/finaltech/core/listener/ShineListener.java
@@ -102,7 +102,7 @@ public class ShineListener implements Listener {
                             }
                         }
                     }
-                    player.setHealth(Math.max(player.getHealth() * 0.9 - player.getMaxHealth() * 0.1 * equipmentCount - player.getMaxHealth() * 0.05 * effectCount - shineCount * 0.15, 0));
+                    player.setHealth(Math.max(player.getHealth() * 0.9 - player.getMaxHealth() * 0.1 * equipmentCount - player.getMaxHealth() * 0.05 * effectCount - shineCount * 1.5, 0));
 
                     TickerTaskRunner.applyOrAddTo(new UntreatableEffect(shineCount * 20 + 20, 1), player, FinalTech.getInstance());
                 }
@@ -112,7 +112,6 @@ public class ShineListener implements Listener {
 
     @EventHandler
     public void onEntityTeleport(EntityTeleportEvent entityTeleportEvent) {
-        System.out.println("teleport");
         Location sourceLocation = entityTeleportEvent.getFrom();
         if (sourceLocation.getWorld() != null) {
             World world = sourceLocation.getWorld();
