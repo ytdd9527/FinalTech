c7d90524182b241f09cc772eab661869f379b635
ljc0424 <1979328077@qq.com>
2023-01-06 11:51:27 +0800

    refactor(AreaAccessor): refactor for AreaAccessor

--- src/main/java/io/taraxacum/finaltech/core/menu/accessor/AreaAccessorMenu.java
+++ src/main/java/io/taraxacum/finaltech/core/menu/accessor/AreaAccessorMenu.java
@@ -3,7 +3,6 @@ package io.taraxacum.finaltech.core.menu.accessor;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import io.github.thebusybiscuit.slimefun4.utils.ChestMenuUtils;
-import io.taraxacum.common.util.StringNumberUtil;
 import io.taraxacum.finaltech.FinalTech;
 import io.taraxacum.finaltech.core.helper.Icon;
 import io.taraxacum.finaltech.core.item.machine.AbstractMachine;
@@ -34,7 +33,7 @@ import java.util.Map;
  * @author Final_ROOT
  * @since 2.0
  */
-public class AreaAccessorMenu extends AbstractMachineMenu {
+public class AreaAccessorMenu extends AbstractAccessorMenu {
     private static final int[] BORDER = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53};
     private static final int[] INPUT_BORDER = new int[0];
     private static final int[] OUTPUT_BORDER = new int[0];
@@ -57,25 +56,8 @@ public class AreaAccessorMenu extends AbstractMachineMenu {
     }
 
     @Override
-    public void newInstance(@Nonnull BlockMenu blockMenu, @Nonnull Block block) {
-        super.newInstance(blockMenu, block);
-        blockMenu.addMenuOpeningHandler(p -> {
-            Location location = block.getLocation();
-            String value = BlockStorage.getLocationInfo(location, AreaAccessor.KEY);
-            if (value == null) {
-                value = AreaAccessor.THRESHOLD;
-            }
-            if (StringNumberUtil.compare(value, StringNumberUtil.ZERO) <= 0) {
-                p.closeInventory();
-                return;
-            }
-            BlockStorage.addBlockInfo(block, AreaAccessor.KEY, StringNumberUtil.sub(value));
-            blockMenu.close();
-
-            if (p.getPlayer() != null) {
-                AreaAccessorMenu.this.generateMenu(p.getPlayer(), location, AreaAccessor.RANGE, 0);
-            }
-        });
+    protected void doFunction(@Nonnull BlockMenu blockMenu, @Nonnull Block block, @Nonnull Player player) {
+        this.generateMenu(player, block.getLocation(), AreaAccessor.RANGE, 0);
     }
 
     @Override
@@ -111,6 +93,7 @@ public class AreaAccessorMenu extends AbstractMachineMenu {
     /**
      * @param page begin from 0
      */
+    // TODO async
     public void generateMenu(@Nonnull Player player, @Nonnull Location location, int range, int page) {
         World world = location.getWorld();
         if (world == null) {
