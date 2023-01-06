61d5bbfa10a544411bccaa235f1e6b0f03421041
ljc0424 <1979328077@qq.com>
2023-01-06 11:45:25 +0800

    docs(DigitalItem): added comments for DigitalItem.java

--- src/main/java/io/taraxacum/finaltech/core/interfaces/DigitalItem.java
+++ src/main/java/io/taraxacum/finaltech/core/interfaces/DigitalItem.java
@@ -1,10 +1,15 @@
 package io.taraxacum.finaltech.core.interfaces;
 
 /**
+ * A Slimefun item that could be marked a specified number in some place.
+ * @see io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem
  * @author Final_ROOT
  * @since 2.0
  */
 public interface DigitalItem {
 
+    /**
+     * @return The value of the item.
+     */
     int getDigit();
 }
