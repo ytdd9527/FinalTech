87304719d1826c83ffb582da5158124d74a502ca
ljc0424 <1979328077@qq.com>
2023-02-03 09:41:12 +0800

    feat: add an icon in the item group menu

--- src/main/java/io/taraxacum/finaltech/core/group/MainItemGroup.java
+++ src/main/java/io/taraxacum/finaltech/core/group/MainItemGroup.java
@@ -12,7 +12,6 @@ import io.github.thebusybiscuit.slimefun4.utils.ChestMenuUtils;
 import io.taraxacum.finaltech.FinalTech;
 import io.taraxacum.libs.slimefun.util.GuideUtil;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ChestMenu;
-import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -30,7 +29,8 @@ public class MainItemGroup extends FlexItemGroup {
     private static final int BACK_SLOT = 1;
     private static final int PREVIOUS_SLOT = 3;
     private static final int NEXT_SLOT = 5;
-    private static final int[] BORDER = new int[] {0, 2, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+    private static final int ICON_SLOT = 7;
+    private static final int[] BORDER = new int[] {0, 2, 4, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
     private static final int[] MAIN_CONTENT = new int[] {18, 27, 36, 45};
     private static final int[][] SUB_CONTENT = new int[][] {
             new int[] {19, 20, 21, 22, 23, 24, 25, 26},
@@ -66,7 +66,7 @@ public class MainItemGroup extends FlexItemGroup {
 
     @Override
     public void open(@Nonnull Player player, @Nonnull PlayerProfile playerProfile, @Nonnull SlimefunGuideMode slimefunGuideMode) {
-        playerProfile.getGuideHistory().add(MainItemGroup.this, this.page);
+        playerProfile.getGuideHistory().add(this, this.page);
         this.generateMenu(player, playerProfile, slimefunGuideMode).open(player);
     }
 
@@ -92,13 +92,13 @@ public class MainItemGroup extends FlexItemGroup {
 
     @Nonnull
     private ChestMenu generateMenu(@Nonnull Player player, @Nonnull PlayerProfile playerProfile, @Nonnull SlimefunGuideMode slimefunGuideMode) {
-        ChestMenu chestMenu = new ChestMenu(FinalTech.getLanguageString(FinalTech.class.getSimpleName()));
+        ChestMenu chestMenu = new ChestMenu(FinalTech.getLanguageString(FinalTech.class.getSimpleName()) + "-" + FinalTech.getConfigManager().getOrDefault("unknown", "version"));
 
         chestMenu.setEmptySlotsClickable(false);
         chestMenu.addMenuOpeningHandler(pl -> pl.playSound(pl.getLocation(), Sound.ITEM_BOOK_PAGE_TURN, 1, 1));
 
         chestMenu.addItem(BACK_SLOT, ChestMenuUtils.getBackButton(player));
-        chestMenu.addMenuClickHandler(1, (pl, s, is, action) -> {
+        chestMenu.addMenuClickHandler(BACK_SLOT, (pl, s, is, action) -> {
             GuideHistory guideHistory = playerProfile.getGuideHistory();
             if (action.isShiftClicked()) {
                 SlimefunGuide.openMainMenu(playerProfile, slimefunGuideMode, guideHistory.getMainMenuPage());
@@ -111,18 +111,21 @@ public class MainItemGroup extends FlexItemGroup {
         chestMenu.addItem(PREVIOUS_SLOT, ChestMenuUtils.getPreviousButton(player, this.page, (this.fatherItemGroupList.size() - 1) / MAIN_CONTENT.length + 1));
         chestMenu.addMenuClickHandler(PREVIOUS_SLOT, (p, slot, item, action) -> {
             GuideUtil.removeLastEntry(playerProfile.getGuideHistory());
-            MainItemGroup mainItemGroup = MainItemGroup.this.getByPage(Math.max(MainItemGroup.this.page - 1, 1));
-            Bukkit.getScheduler().runTask(FinalTech.getInstance(), () -> mainItemGroup.open(player, playerProfile, slimefunGuideMode));
+            MainItemGroup mainItemGroup = this.getByPage(Math.max(this.page - 1, 1));
+            mainItemGroup.open(player, playerProfile, slimefunGuideMode);
             return false;
         });
 
         chestMenu.addItem(NEXT_SLOT, ChestMenuUtils.getNextButton(player, this.page, (this.fatherItemGroupList.size() - 1) / MAIN_CONTENT.length + 1));
         chestMenu.addMenuClickHandler(NEXT_SLOT, (p, slot, item, action) -> {
             GuideUtil.removeLastEntry(playerProfile.getGuideHistory());
-            MainItemGroup mainItemGroup = MainItemGroup.this.getByPage(Math.min(MainItemGroup.this.page + 1, (this.fatherItemGroupList.size() - 1) / MAIN_CONTENT.length + 1));
-            Bukkit.getScheduler().runTask(FinalTech.getInstance(), () -> mainItemGroup.open(player, playerProfile, slimefunGuideMode));
+            MainItemGroup mainItemGroup = this.getByPage(Math.min(this.page + 1, (this.fatherItemGroupList.size() - 1) / MAIN_CONTENT.length + 1));
+            mainItemGroup.open(player, playerProfile, slimefunGuideMode);
             return false;
         });
+
+        chestMenu.addItem(ICON_SLOT, super.item);
+        chestMenu.addMenuClickHandler(ICON_SLOT, ChestMenuUtils.getEmptyClickHandler());
 
         for (int slot : BORDER) {
             chestMenu.addItem(slot, ChestMenuUtils.getBackground());
@@ -134,9 +137,9 @@ public class MainItemGroup extends FlexItemGroup {
                 chestMenu.addItem(MAIN_CONTENT[i % MAIN_CONTENT.length], this.fatherItemGroupList.get(i).getItem(player));
                 final int index = i;
                 chestMenu.addMenuClickHandler(MAIN_CONTENT[i % MAIN_CONTENT.length], (p, slot, item, action) -> {
-                    ItemGroup itemGroup = MainItemGroup.this.fatherItemGroupList.get(index);
-                    if (itemGroup instanceof FlexItemGroup) {
-                        Bukkit.getScheduler().runTask(FinalTech.getInstance(), () -> ((FlexItemGroup) itemGroup).open(player, playerProfile, slimefunGuideMode));
+                    ItemGroup itemGroup = this.fatherItemGroupList.get(index);
+                    if (itemGroup instanceof FlexItemGroup flexItemGroup) {
+                        flexItemGroup.open(player, playerProfile, slimefunGuideMode);
                     }
                     return false;
                 });
@@ -147,8 +150,8 @@ public class MainItemGroup extends FlexItemGroup {
                     final int subIndex = j;
                     chestMenu.addMenuClickHandler(SUB_CONTENT[i % MAIN_CONTENT.length][j], (p, slot, item, action) -> {
                         ItemGroup itemGroup = subItemGroupList.get(subIndex);
-                        if (itemGroup instanceof FlexItemGroup) {
-                            Bukkit.getScheduler().runTask(FinalTech.getInstance(), () -> ((FlexItemGroup) itemGroup).open(player, playerProfile, slimefunGuideMode));
+                        if (itemGroup instanceof FlexItemGroup flexItemGroup) {
+                            flexItemGroup.open(player, playerProfile, slimefunGuideMode);
                         }
                         return false;
                     });
--- src/main/java/io/taraxacum/finaltech/core/group/SubFlexItemGroup.java
+++ src/main/java/io/taraxacum/finaltech/core/group/SubFlexItemGroup.java
@@ -22,6 +22,7 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
@@ -38,12 +39,15 @@ public class SubFlexItemGroup extends FlexItemGroup {
     private static final int BACK_SLOT = 1;
     private static final int PREVIOUS_SLOT = 3;
     private static final int NEXT_SLOT = 5;
-    private static final int[] BORDER = new int[] {0, 2, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+    private static final int ICON_SLOT = 7;
+    private static final int[] BORDER = new int[] {0, 2, 4, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
     private static final int[][] MAIN_CONTENT_L = new int[][] {
             new int[] {18, 19, 20, 21, 22, 23, 24, 25, 26},
             new int[] {27, 28, 29, 30, 31, 32, 33, 34, 35},
             new int[] {36, 37, 38, 39, 40, 41, 42, 43, 44},
             new int[] {45, 46, 47, 48, 49, 50, 51, 52, 53}};
+
+    private static final JavaPlugin JAVA_PLUGIN = FinalTech.getInstance();
 
     /**
      * One SlimefunItem List should only contain 9 SlimefunItems at most.
@@ -145,7 +149,7 @@ public class SubFlexItemGroup extends FlexItemGroup {
 
     @Nonnull
     private ChestMenu generateMenu(@Nonnull Player player, @Nonnull PlayerProfile playerProfile, @Nonnull SlimefunGuideMode slimefunGuideMode) {
-        ChestMenu chestMenu = new ChestMenu(FinalTech.getLanguageString(FinalTech.class.getSimpleName()));
+        ChestMenu chestMenu = new ChestMenu(ItemStackUtil.getItemName(super.item));
 
         chestMenu.setEmptySlotsClickable(false);
         chestMenu.addMenuOpeningHandler(pl -> pl.playSound(pl.getLocation(), Sound.ITEM_BOOK_PAGE_TURN, 1, 1));
@@ -164,18 +168,21 @@ public class SubFlexItemGroup extends FlexItemGroup {
         chestMenu.addItem(PREVIOUS_SLOT, ChestMenuUtils.getPreviousButton(player, this.page, (slimefunItemList.size() - 1) / MAIN_CONTENT_L.length + 1));
         chestMenu.addMenuClickHandler(PREVIOUS_SLOT, (p, slot, item, action) -> {
             GuideUtil.removeLastEntry(playerProfile.getGuideHistory());
-            SubFlexItemGroup subFlexItemGroup = SubFlexItemGroup.this.getByPage(Math.max(SubFlexItemGroup.this.page - 1, 1));
-            Bukkit.getScheduler().runTask(FinalTech.getInstance(), () -> subFlexItemGroup.open(player, playerProfile, slimefunGuideMode));
+            SubFlexItemGroup subFlexItemGroup = this.getByPage(Math.max(this.page - 1, 1));
+            subFlexItemGroup.open(player, playerProfile, slimefunGuideMode);
             return false;
         });
 
         chestMenu.addItem(NEXT_SLOT, ChestMenuUtils.getNextButton(player, this.page, (slimefunItemList.size() - 1) / MAIN_CONTENT_L.length + 1));
         chestMenu.addMenuClickHandler(NEXT_SLOT, (p, slot, item, action) -> {
             GuideUtil.removeLastEntry(playerProfile.getGuideHistory());
-            SubFlexItemGroup subFlexItemGroup = SubFlexItemGroup.this.getByPage(Math.min(this.page + 1, (slimefunItemList.size() - 1) / MAIN_CONTENT_L.length + 1));
-            Bukkit.getScheduler().runTask(FinalTech.getInstance(), () -> subFlexItemGroup.open(player, playerProfile, slimefunGuideMode));
+            SubFlexItemGroup subFlexItemGroup = this.getByPage(Math.min(this.page + 1, (slimefunItemList.size() - 1) / MAIN_CONTENT_L.length + 1));
+            subFlexItemGroup.open(player, playerProfile, slimefunGuideMode);
             return false;
         });
+
+        chestMenu.addItem(ICON_SLOT, super.item);
+        chestMenu.addMenuClickHandler(ICON_SLOT, ChestMenuUtils.getEmptyClickHandler());
 
         for (int slot : BORDER) {
             chestMenu.addItem(slot, ChestMenuUtils.getBackground());
@@ -183,7 +190,7 @@ public class SubFlexItemGroup extends FlexItemGroup {
         }
 
         for (int i = 0; i < MAIN_CONTENT_L.length; i++) {
-            int index = i + SubFlexItemGroup.this.page * MAIN_CONTENT_L.length - MAIN_CONTENT_L.length;
+            int index = i + this.page * MAIN_CONTENT_L.length - MAIN_CONTENT_L.length;
             if (index < slimefunItemList.size()) {
                 List<SlimefunItem> slimefunItemList = this.slimefunItemList.get(index);
                 for (int j = 0; j < slimefunItemList.size(); j++) {
@@ -196,7 +203,7 @@ public class SubFlexItemGroup extends FlexItemGroup {
                         chestMenu.addMenuClickHandler(MAIN_CONTENT_L[i][j], (p, slot, item, action) -> {
                             RecipeItemGroup recipeItemGroup = RecipeItemGroup.getByItemStack(player, playerProfile, slimefunGuideMode, slimefunItem.getItem());
                             if (recipeItemGroup != null) {
-                                Bukkit.getScheduler().runTask(FinalTech.getInstance(), () -> recipeItemGroup.open(player, playerProfile, slimefunGuideMode));
+                                Bukkit.getScheduler().runTask(JAVA_PLUGIN, () -> recipeItemGroup.open(player, playerProfile, slimefunGuideMode));
                             }
                             return false;
                         });
@@ -216,7 +223,7 @@ public class SubFlexItemGroup extends FlexItemGroup {
 
                             if (!event.isCancelled() && !playerProfile.hasUnlocked(research)) {
                                 if (research.canUnlock(player)) {
-                                    new SurvivalSlimefunGuide(false, false).unlockItem(player, slimefunItem, player1 -> SubFlexItemGroup.this.refresh(player, playerProfile, slimefunGuideMode));
+                                    new SurvivalSlimefunGuide(false, false).unlockItem(player, slimefunItem, player1 -> this.refresh(player, playerProfile, slimefunGuideMode));
                                 } else {
                                     this.refresh(player, playerProfile, slimefunGuideMode);
                                     Slimefun.getLocalization().sendMessage(player, "messages.not-enough-xp", true);
@@ -244,7 +251,7 @@ public class SubFlexItemGroup extends FlexItemGroup {
                 if (this.pageMap.containsKey(page)) {
                     return this.pageMap.get(page);
                 }
-                SubFlexItemGroup subFlexItemGroup = new SubFlexItemGroup(new NamespacedKey(FinalTech.getInstance(), this.getKey().getKey() + "_" + page), this.item, this.getTier(), page);
+                SubFlexItemGroup subFlexItemGroup = new SubFlexItemGroup(new NamespacedKey(JAVA_PLUGIN, this.getKey().getKey() + "_" + page), this.item, this.getTier(), page);
                 subFlexItemGroup.slimefunItemList = this.slimefunItemList;
                 subFlexItemGroup.pageMap = this.pageMap;
                 this.pageMap.put(page, subFlexItemGroup);
@@ -253,9 +260,10 @@ public class SubFlexItemGroup extends FlexItemGroup {
         }
     }
 
+    // TODO
     @Nonnull
     public static SubFlexItemGroup generateFromItemGroup(@Nonnull ItemGroup itemGroup, @Nonnull Player player) {
-        SubFlexItemGroup subFlexItemGroup = new SubFlexItemGroup(new NamespacedKey(FinalTech.getInstance(), itemGroup.getKey().getNamespace()), itemGroup.getItem(player), itemGroup.getTier());
+        SubFlexItemGroup subFlexItemGroup = new SubFlexItemGroup(new NamespacedKey(JAVA_PLUGIN, itemGroup.getKey().getNamespace()), itemGroup.getItem(player), itemGroup.getTier());
         subFlexItemGroup.addTo(itemGroup.getItems());
         return subFlexItemGroup;
     }
