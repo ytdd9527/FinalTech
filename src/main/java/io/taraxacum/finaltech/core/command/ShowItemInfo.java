a06ef1e0a36c383e7f306863ab35d98cc16b5b89
ljc0424 <1979328077@qq.com>
2023-02-06 20:48:02 +0800

    feat: better experience for command show item info

--- src/main/java/io/taraxacum/finaltech/core/command/ShowItemInfo.java
+++ src/main/java/io/taraxacum/finaltech/core/command/ShowItemInfo.java
@@ -5,6 +5,7 @@ import io.github.thebusybiscuit.slimefun4.api.player.PlayerProfile;
 import io.github.thebusybiscuit.slimefun4.core.guide.GuideHistory;
 import io.github.thebusybiscuit.slimefun4.core.guide.SlimefunGuide;
 import io.github.thebusybiscuit.slimefun4.core.guide.SlimefunGuideMode;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.taraxacum.finaltech.FinalTech;
 import io.taraxacum.libs.slimefun.dto.ItemValueTable;
 import io.taraxacum.finaltech.core.group.RecipeItemGroup;
@@ -30,12 +31,23 @@ public class ShowItemInfo implements CommandExecutor {
         if (commandSender instanceof Player player) {
             ItemStack item = player.getItemInHand();
             SlimefunItem slimefunItem = SlimefunItem.getByItem(item);
-            if (slimefunItem == null || slimefunItem.isDisabled() || !slimefunItem.canUse(player, false) || slimefunItem.isHidden()) {
-                player.sendRawMessage(FinalTech.getLanguageString("message", "no-permission", "item"));
+            if(slimefunItem == null) {
+                player.sendRawMessage(FinalTech.getLanguageString("message", "invalid-item"));
                 return true;
             }
+
             Optional<PlayerProfile> playerProfile = PlayerProfile.find(player);
             if (playerProfile.isPresent()) {
+                if(!playerProfile.get().hasUnlocked(slimefunItem.getResearch())) {
+                    Slimefun.getLocalization().sendMessage(player, "messages.not-researched", true, s1 -> s1.replace("%item%", slimefunItem.getItemName()));
+                    return true;
+                }
+
+                if (!slimefunItem.canUse(player, false) || slimefunItem.isHidden()) {
+                    player.sendRawMessage(FinalTech.getLanguageString("message", "no-permission", "item"));
+                    return true;
+                }
+
                 RecipeItemGroup recipeItemGroup = RecipeItemGroup.getByItemStack(player, playerProfile.get(), SlimefunGuideMode.SURVIVAL_MODE, item);
                 if (recipeItemGroup != null) {
                     GuideHistory guideHistory = playerProfile.get().getGuideHistory();
