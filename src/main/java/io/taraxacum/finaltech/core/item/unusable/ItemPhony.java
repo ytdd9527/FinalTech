8800a702e909d038a4345b5de085007693d3aaa0
ljc0424 <1979328077@qq.com>
2023-03-13 10:00:55 +0800

    feat: one more ValidItem

--- src/main/java/io/taraxacum/finaltech/core/item/unusable/ItemPhony.java
+++ src/main/java/io/taraxacum/finaltech/core/item/unusable/ItemPhony.java
@@ -5,33 +5,41 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.taraxacum.finaltech.FinalTech;
 import io.taraxacum.finaltech.core.interfaces.RecipeItem;
-import io.taraxacum.finaltech.setup.FinalTechItemStacks;
+import io.taraxacum.libs.plugin.dto.ItemWrapper;
 import io.taraxacum.libs.plugin.util.ItemStackUtil;
 import io.taraxacum.finaltech.util.RecipeUtil;
-import org.bukkit.entity.Player;
+import io.taraxacum.libs.slimefun.interfaces.SimpleValidItem;
+import io.taraxacum.libs.slimefun.util.SfItemUtil;
 import org.bukkit.inventory.ItemStack;
 
-import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
 
 /**
  * @author Final_ROOT
  * @since 2.0
  */
-public class ItemPhony extends UnusableSlimefunItem implements RecipeItem {
+public class ItemPhony extends UnusableSlimefunItem implements RecipeItem, SimpleValidItem {
+    private final ItemWrapper templateValidItem = new ItemWrapper(this.getValidItem());
+
     public ItemPhony(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
-    }
-
-    public static boolean isValid(@Nullable ItemStack item) {
-        return ItemStackUtil.isItemSimilar(item, FinalTechItemStacks.PHONY);
-    }
-
-    public static ItemStack newItem(@Nullable ItemStack item1, @Nullable ItemStack item2, @Nullable Player player) {
-        return ItemStackUtil.cloneItem(FinalTechItemStacks.PHONY);
     }
 
     @Override
     public void registerDefaultRecipes() {
         RecipeUtil.registerDescriptiveRecipe(FinalTech.getLanguageManager(), this);
+    }
+
+    @Nonnull
+    @Override
+    public ItemStack getValidItem() {
+        ItemStack validItem = new ItemStack(this.getItem());
+        SfItemUtil.setSpecialItemKey(validItem);
+        return validItem;
+    }
+
+    @Override
+    public boolean verifyItem(@Nonnull ItemStack itemStack) {
+        return ItemStackUtil.isItemSimilar(itemStack, this.templateValidItem);
     }
 }
