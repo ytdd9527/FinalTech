1cf6bf09120793b59d4079bcc3c2e77bb96ea7e4
ljc0424 <1979328077@qq.com>
2023-03-13 10:13:31 +0800

    bugfix: fix potential bug for 'Box'

--- src/main/java/io/taraxacum/finaltech/core/listener/BoxListener.java
+++ src/main/java/io/taraxacum/finaltech/core/listener/BoxListener.java
@@ -2,10 +2,9 @@ package io.taraxacum.finaltech.core.listener;
 
 import io.github.thebusybiscuit.slimefun4.api.player.PlayerProfile;
 import io.github.thebusybiscuit.slimefun4.api.researches.Research;
-import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.taraxacum.finaltech.FinalTech;
 import io.taraxacum.finaltech.core.item.unusable.Box;
-import io.taraxacum.finaltech.setup.FinalTechItemStacks;
+import io.taraxacum.finaltech.setup.FinalTechItems;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -14,6 +13,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,12 +35,14 @@ public class BoxListener implements Listener {
                 if(lastDamageEvent != null && EntityDamageEvent.DamageCause.SUICIDE.equals(lastDamageEvent.getCause())) {
                     Optional<PlayerProfile> playerProfile = PlayerProfile.find(player);
                     if(playerProfile.isPresent()) {
-                        List<Research> researchList = Slimefun.getRegistry().getResearches();
-                        playerProfile.get().setResearched(researchList.get(FinalTech.getRandom().nextInt(researchList.size())), false);
+                        List<Research> researchList = new ArrayList<>(playerProfile.get().getResearches());
+                        if(!researchList.isEmpty()) {
+                            playerProfile.get().setResearched(researchList.get(FinalTech.getRandom().nextInt(researchList.size())), false);
+                        }
                         player.sendMessage(FinalTech.getLanguageString("items", "FINALTECH_BOX", "message"));
                     }
                 } else {
-                    world.dropItem(location, FinalTechItemStacks.BOX);
+                    world.dropItem(location, FinalTechItems.BOX.getValidItem());
                 }
             }
         }
