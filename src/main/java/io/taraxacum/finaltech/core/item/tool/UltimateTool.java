0376d6e31cccfe6b5b95deb15866eaf982dae14b
ljc0424 <1979328077@qq.com>
2023-03-15 14:10:29 +0800

    feat: add cd for UltimateTool

--- src/main/java/io/taraxacum/finaltech/core/item/tool/UltimateTool.java
+++ src/main/java/io/taraxacum/finaltech/core/item/tool/UltimateTool.java
@@ -5,18 +5,32 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.handlers.ToolUseHandler;
 import io.taraxacum.finaltech.core.item.AbstractMySlimefunItem;
+import io.taraxacum.finaltech.util.ConfigUtil;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Final_ROOT
  * @since 2.0
  */
 public abstract class UltimateTool extends AbstractMySlimefunItem {
+    private final Map<Player, Long> timeMap = new HashMap<>();
+    private final long intervalThreshold = ConfigUtil.getOrDefaultItemSetting(40, this, "interval-threshold");
+
     public UltimateTool(@Nonnull ItemGroup itemGroup, @Nonnull SlimefunItemStack item, @Nonnull RecipeType recipeType, @Nonnull ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
         this.addItemHandler((ToolUseHandler) (blockBreakEvent, itemStack, fortune, drops) -> {
+            Player player = blockBreakEvent.getPlayer();
+            Long lastTime = this.timeMap.get(player);
+            long nowTime = System.currentTimeMillis();
+            if(nowTime - lastTime < this.intervalThreshold) {
+                return;
+            }
+
             blockBreakEvent.setDropItems(false);
             int count = 1;
             for(ItemStack drop : drops) {
@@ -24,6 +38,8 @@ public abstract class UltimateTool extends AbstractMySlimefunItem {
             }
             count = (int)Math.pow(count, 0.5);
             blockBreakEvent.setExpToDrop(blockBreakEvent.getExpToDrop() + count);
+
+            this.timeMap.put(player, nowTime);
         });
     }
 }
