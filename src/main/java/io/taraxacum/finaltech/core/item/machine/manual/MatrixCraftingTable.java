482a0fad066cfc83857120b252b19e4b5bbc4b05
ljc0424 <1979328077@qq.com>
2023-03-26 19:33:28 +0800

    feat: register display recipe for MatrixCraftingTable

--- src/main/java/io/taraxacum/finaltech/core/item/machine/manual/MatrixCraftingTable.java
+++ src/main/java/io/taraxacum/finaltech/core/item/machine/manual/MatrixCraftingTable.java
@@ -5,11 +5,13 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.taraxacum.finaltech.FinalTech;
+import io.taraxacum.finaltech.core.helper.Icon;
 import io.taraxacum.finaltech.core.interfaces.RecipeItem;
 import io.taraxacum.finaltech.core.menu.manual.AbstractManualMachineMenu;
 import io.taraxacum.finaltech.core.menu.manual.MatrixCraftingTableMenu;
 import io.taraxacum.finaltech.setup.FinalTechRecipeTypes;
 import io.taraxacum.finaltech.util.RecipeUtil;
+import io.taraxacum.libs.slimefun.dto.RecipeTypeRegistry;
 import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
@@ -17,6 +19,8 @@ import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Final_ROOT
@@ -46,5 +50,16 @@ public class MatrixCraftingTable extends AbstractManualMachine implements Recipe
         RecipeUtil.registerDescriptiveRecipeWithBorder(FinalTech.getLanguageManager(), this);
 
         RecipeUtil.registerRecipeByRecipeType(this, FinalTechRecipeTypes.MATRIX_CRAFTING_TABLE);
+    }
+
+    @Nonnull
+    @Override
+    public List<ItemStack> getDisplayRecipes() {
+        List<ItemStack> itemStackList = new ArrayList<>();
+        for(SlimefunItem slimefunItem : RecipeTypeRegistry.getInstance().getByRecipeType(FinalTechRecipeTypes.MATRIX_CRAFTING_TABLE)) {
+            itemStackList.add(Icon.BORDER_ICON);
+            itemStackList.add(slimefunItem.getRecipeOutput());
+        }
+        return itemStackList;
     }
 }
